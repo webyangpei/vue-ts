6808b70bf87b3cc6cd584f842f03f767c591c317
wb_yangpei <wb_yangpei@kuaishou.com>
2020-10-19 17:03:49 +0800

    refactor: improve code

---
+++ src/store/app/mutations.ts
@@ -0,0 +1,13 @@
+import { MutationTree} from "vuex";
+import { AppState } from "@/store/app/types";
+
+export const mutations:MutationTree<AppState> = {
+    profileLoaded(state, payload: any) {
+        // state.device = false;
+        // state.sidebar = payload;
+    },
+    profileError(state) {
+        // state.device = true;
+        // state.sidebar = undefined;
+    }
+}
---
+++ src/store/app/types.ts
@@ -0,0 +1,8 @@
+// export interface permission_router {
+//
+// }
+export interface AppState {
+    device: string,
+    sidebar: string
+    // permission_routers?: permission_router
+}
